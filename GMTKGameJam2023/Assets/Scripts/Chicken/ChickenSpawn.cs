ee7e9e678e357e0d7a083865e560175f0f24ad58
Jake Horner <jake.horner@ergogroup.ie>
2023-07-07 23:05:46 +0100

    chickens spawning random

--- GMTKGameJam2023/Assets/Scripts/Chicken/ChickenSpawn.cs
+++ GMTKGameJam2023/Assets/Scripts/Chicken/ChickenSpawn.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class ChickenSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    public SpawningPoint[] spawnSpots;
+
+    public GameObject ChickenPrefab;
+
+    public float minSpawnTime = 3f;
+    public float maxSpawnTime = 6f;
+
     void Start()
     {
-        
+        spawnChicken(spawnSpots[Random.Range(1, spawnSpots.Length)]);
+        startSpawn();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void startSpawn(){
+        float spawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+        IEnumerator coroutine = WaitAndSpawn(spawnTime);
+        StartCoroutine(coroutine);
     }
+
+    IEnumerator WaitAndSpawn(float moveTime)
+    {
+        yield return new WaitForSeconds(moveTime);
+        int selected = basedRandom();
+        // Debug.Log(basedRandom());
+        spawnChicken(spawnSpots[selected]);
+        //restart timer
+        startSpawn();
+    }
+
+    void spawnChicken(SpawningPoint point){
+        // Debug.Log("Spawning Chickens");
+        // foreach (SpawningPoint point in spawnSpots){
+        //     Instantiate(ChickenPrefab, point.position, Quaternion.identity);
+        // }
+        Instantiate(ChickenPrefab, point.position, Quaternion.identity);
+    }
+
+    int basedRandom(){
+        return Random.Range(1, spawnSpots.Length);
+        //we could use spawnProbability in SpawningPoint object to create smarter probability
+    }
+}
+[System.Serializable]
+public class SpawningPoint {
+    public Vector3 position;
+    public float spawnProbability;
 }
