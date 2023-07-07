a24c493e020398a82b45b510aa71e3e180c6db03
TheJackXBL <79862200+TheJackXBL@users.noreply.github.com>
2023-07-07 21:24:05 +0100

    Car Prefab

---
+++ GMTKGameJam2023/Assets/Scripts/Car/Car.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Car : MonoBehaviour
+{
+
+    [Header("Speed")]
+    [SerializeField] protected float carSpeed = 5f;
+
+    protected virtual void SetCarSpeed()
+    {
+        gameObject.TryGetComponent(out Rigidbody2D rb);
+
+        if (rb != null)
+        {
+            rb.velocity = new Vector2(0, carSpeed);
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Death Box")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GMTKGameJam2023/Assets/Scripts/Car/StandardCar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardCar : Car
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCarSpeed();
+    }
+
+    
+}
