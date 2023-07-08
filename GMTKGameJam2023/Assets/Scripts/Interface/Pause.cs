6897684d57e151b4ebe95e650995d8198a2c90d2
AnahatM <anahat.mudgal@gmail.com>
2023-07-07 23:37:45 -0400

    Working Pause Functionality

---
+++ GMTKGameJam2023/Assets/Scripts/Interface/Pause.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject pauseUI;
+
+    [Header("User Input")]
+    [SerializeField] private KeyCode pauseKey = KeyCode.P;
+    [SerializeField] private KeyCode pauseKeyAlt = KeyCode.Escape;
+
+    [Header("Pause Values")]
+    [SerializeField] private float pauseTimeScale = 0;
+
+    private float startTimeScale = 1;
+
+    public bool isPaused = false;
+
+    private void Start()
+    {
+        startTimeScale = Time.timeScale;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(pauseKey) || Input.GetKeyDown(pauseKeyAlt))
+        {
+            if (isPaused)
+                UnpauseGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = startTimeScale;
+        pauseUI.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = pauseTimeScale;
+        pauseUI.SetActive(true);
+    }
+}
