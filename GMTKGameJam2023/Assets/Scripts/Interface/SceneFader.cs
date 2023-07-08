9cbc18a725038124cd6ab8951dc05c49f6b81290
AnahatM <anahat.mudgal@gmail.com>
2023-07-07 23:01:36 -0400

    Main Menu and Scene Fading Functionality

---
+++ GMTKGameJam2023/Assets/Scripts/Interface/SceneFader.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneFader : MonoBehaviour
+{
+    [Header("Scene Loading")]
+    [SerializeField] private string mainMenuSceneName = "MainMenu";
+    [SerializeField] private string gameSceneName = "MainGame";
+
+    [Header("References")]
+    [SerializeField] private Image fadeImage;
+
+    [Header("Transition Values")]
+    [SerializeField] private AnimationCurve fadeAnimationCurve;
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeTo(string targetScene)
+    {
+        // Smoothly Transition Between Scenes
+        StartCoroutine(FadeOut(targetScene));
+    }
+
+    // Scene Loading Functions
+    public void ReloadScene() => FadeTo(SceneManager.GetActiveScene().name);
+    public void FadeToMainMenu() => FadeTo(mainMenuSceneName);
+    public void FadeToGame() => FadeTo(gameSceneName);
+
+    private IEnumerator FadeIn()
+    {
+        // Fade In Using Animation Curve
+        float t = 1f;
+        while (t > 0f)
+        {
+            t -= Time.deltaTime;
+            float a = fadeAnimationCurve.Evaluate(t);
+            fadeImage.color = new Color(0, 0, 0, a);
+            yield return 0;
+        }
+    }
+
+    private IEnumerator FadeOut(string targetScene)
+    {
+        // Fade Out Using Reversed Animation Curve
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            float a = fadeAnimationCurve.Evaluate(t);
+            fadeImage.color = new Color(0, 0, 0, a);
+            yield return 0;
+        }
+
+        // Load Target Scene
+        SceneManager.LoadScene(targetScene);
+    }
+}
