9a56b1b50b6e2352d2c4470346fe38e72f77b451
Jake Horner <jake.horner@ergogroup.ie>
2023-07-08 18:19:58 +0100

    speed up start

--- GMTKGameJam2023/Assets/Scripts/GameManager.cs
+++ GMTKGameJam2023/Assets/Scripts/GameManager.cs
@@ -10,9 +10,13 @@ public class GameManager : MonoBehaviour
     public int playerScore = 0;
     public int tokens = 0;
     public float time = 120f;
+    public int intesitySetting = 0;
+
+    private SoundManager soundManager;
     
     private void Start()
     {
+        soundManager = FindObjectOfType<SoundManager>();
         safelyCrossedChickens = 0;
         killCount = 0;
         playerScore = 0;
@@ -24,10 +28,27 @@ public class GameManager : MonoBehaviour
 
     private void setTime(){
         time -= Time.deltaTime;
-        if(time % 30f == 0){
-            Debug.Log("30 seconds");
+        if(time <= 90f && intesitySetting == 0){
+            intesitySetting ++;
+            Debug.Log("90 seconds");
+            soundManager.PlaySound(SoundManager.SoundType.GameSpeed);
         }
-        if ( time <= 0 )
+        if(time <= 60f && intesitySetting == 1){
+            intesitySetting ++;
+            Debug.Log("60 seconds");
+            soundManager.PlaySound(SoundManager.SoundType.GameSpeed);
+        }
+        if(time <= 40f && intesitySetting == 2){
+            intesitySetting ++;
+            Debug.Log("40 seconds");
+            soundManager.PlaySound(SoundManager.SoundType.GameSpeed);
+        }
+        if(time <= 20f && intesitySetting == 3){
+            intesitySetting ++;
+            Debug.Log("20 seconds");
+            soundManager.PlaySound(SoundManager.SoundType.GameSpeed);
+        }
+        if ( time <= 0)
         {
             // send to results screen
         }
