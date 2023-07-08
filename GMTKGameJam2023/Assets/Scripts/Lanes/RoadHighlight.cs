ec37fda093071bd0537131c6912d7f5063b80045
AnahatM <anahat.mudgal@gmail.com>
2023-07-07 20:03:19 -0400

    Fixed Highlights

--- GMTKGameJam2023/Assets/Scripts/Lanes/RoadHighlight.cs
+++ GMTKGameJam2023/Assets/Scripts/Lanes/RoadHighlight.cs
@@ -28,12 +28,17 @@ public class RoadHighlight : MonoBehaviour
         Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
-        if (hit.collider == null) return;
+        if (hit.collider == null)
+        {
+            topHighlight.SetActive(false);
+            bottomHighlight.SetActive(false);
+            return;
+        };
 
-        if (hit.collider.gameObject != gameObject) return;
-
+        bool touchingRoad = hit.collider.gameObject == gameObject;
         bool mouseUp = mousePosition.y > spawnZoneDivider;
-        topHighlight.SetActive(mouseUp);
-        bottomHighlight.SetActive(!mouseUp);
+
+        topHighlight.SetActive(mouseUp && touchingRoad);
+        bottomHighlight.SetActive(!mouseUp && touchingRoad);
     }
 }
