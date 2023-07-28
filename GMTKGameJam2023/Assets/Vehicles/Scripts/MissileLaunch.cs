6a51d2f9031580c68b5ca09efffe6bbc0408605a
AnahatM <anahat.mudgal@gmail.com>
2023-07-27 20:56:29 -0400

    FINALLY FINISHED THE STUPID MISSILE ANIMATION

--- GMTKGameJam2023/Assets/Vehicles/Scripts/MissileLaunch.cs
+++ GMTKGameJam2023/Assets/Vehicles/Scripts/MissileLaunch.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class MissileLaunch : Car
 {
+    [Header("Animation")]
+    [SerializeField] private GameObject missileSprtie;
+
+    public void Explode()
+    {
+        Destroy(missileSprtie.gameObject);
+        GetComponent<Animator>().SetTrigger("Explode");
+    }
+
     public void DestroySelf()
     {
         Destroy(gameObject);
