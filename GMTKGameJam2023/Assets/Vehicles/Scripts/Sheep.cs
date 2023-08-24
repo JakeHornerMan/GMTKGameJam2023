001b63b05d06b081b72e171f3d77012fcb413bfb
AnahatM <anahat.mudgal@gmail.com>
2023-08-24 16:19:29 -0400

    improved car killcount UI & sheep logic

--- GMTKGameJam2023/Assets/Vehicles/Scripts/Sheep.cs
+++ GMTKGameJam2023/Assets/Vehicles/Scripts/Sheep.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 
 public class Sheep : Car
 {
+    [Header("References")]
+    [SerializeField] private Collider2D sheepCollider;
+
+    [Header("Layers")]
+    [SerializeField] private LayerMask waterLayer;
+
+    [Header("Settings")]
+    [SerializeField] private bool allowSheepOnWater = false;
+
     private void Start()
     {
         SetCarSpeed();
 
         soundManager.PlaySheepNoise();
+    }
+
+    private void Update()
+    {
+        if (!allowSheepOnWater && sheepCollider.IsTouchingLayers(waterLayer))
+        {
+            Destroy(gameObject);
+        }
     }
 
     protected override void HandleChickenCollision(ChickenHealth chickenHealth)
