5f2fdf11dabd1cbeb815774521636e59f9f584c7
AnahatM <anahat.mudgal@gmail.com>
2023-09-09 21:53:00 -0400

    assign references

--- GMTKGameJam2023/Assets/Levels/Scripts/LevelCard.cs
+++ GMTKGameJam2023/Assets/Levels/Scripts/LevelCard.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelCard : MonoBehaviour
 {
-    public LevelInfoSO correspondingLevel;
+    [Header("References")]
+    [SerializeField] private GameObject carIconPrefab;
+    [SerializeField] private GameObject chickenIconPrefab;
+
+    [Header("UI References")]
+    [SerializeField] private TextMeshProUGUI lvlNumText;
+    [SerializeField] private TextMeshProUGUI lvlDurationText;
+    [SerializeField] private TextMeshProUGUI lvlNameText;
+    [SerializeField] private TextMeshProUGUI lvlDifficultyText;
+    [SerializeField] private Image lvlDifficultyImage;
+    [SerializeField] private Transform carsInLevelContainer;
+    [SerializeField] private Transform chickenInLevelContainer;
+
+    [HideInInspector] public LevelInfoSO correspondingLevel;
 }
