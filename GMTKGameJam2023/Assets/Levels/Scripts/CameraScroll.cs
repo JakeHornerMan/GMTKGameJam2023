5a3c9f978c8455bec1912ae3f58d16a5d891829d
AnahatM <anahat.mudgal@gmail.com>
2023-09-09 21:09:52 -0400

    only horizontal movement, no zoom on camera world select

--- GMTKGameJam2023/Assets/Levels/Scripts/CameraScroll.cs
+++ GMTKGameJam2023/Assets/Levels/Scripts/CameraScroll.cs
@@ -3,23 +3,14 @@ using UnityEngine;
 
 public class CameraScroll : MonoBehaviour
 {
-    [Header("Camera Zoom Control")]
-    [SerializeField] private float zoomSpeed = 5f;
-    [SerializeField] private float minZoom = 5f;
-    [SerializeField] private float maxZoom = 20f;
-
     [Header("Camera Keyboard Control")]
     [SerializeField] private float cameraScrollSpeed = 1f;
 
     [Header("Keybindings (Arrow Keys)")]
-    [SerializeField] private KeyCode upKey = KeyCode.UpArrow;
-    [SerializeField] private KeyCode downKey = KeyCode.DownArrow;
     [SerializeField] private KeyCode rightKey = KeyCode.RightArrow;
     [SerializeField] private KeyCode leftKey = KeyCode.LeftArrow;
 
     [Header("Keybindings (WASD Keys)")]
-    [SerializeField] private KeyCode upKeyAlt = KeyCode.W;
-    [SerializeField] private KeyCode downKeyAlt = KeyCode.S;
     [SerializeField] private KeyCode rightKeyAlt = KeyCode.D;
     [SerializeField] private KeyCode leftKeyAlt = KeyCode.A;
 
@@ -36,35 +27,17 @@ public class CameraScroll : MonoBehaviour
     private void Update()
     {
         PanCamera();
-        ZoomCamera();
     }
 
     private void PanCamera()
     {
+        // Keyboard movement
         Vector3 moveDirection = Vector3.zero;
-
-        // Check for key inputs (Arrow Keys)
-        if (Input.GetKey(upKey) || Input.GetKey(upKeyAlt))
-        {
-            moveDirection += Vector3.up;
-        }
-        if (Input.GetKey(downKey) || Input.GetKey(downKeyAlt))
-        {
-            moveDirection += Vector3.down;
-        }
         if (Input.GetKey(rightKey) || Input.GetKey(rightKeyAlt))
-        {
             moveDirection += Vector3.right;
-        }
         if (Input.GetKey(leftKey) || Input.GetKey(leftKeyAlt))
-        {
             moveDirection += Vector3.left;
-        }
-
-        // Normalize the direction vector to ensure consistent speed
         moveDirection.Normalize();
-
-        // Move the camera based on the direction and speed
         camTransform.position += moveDirection * cameraScrollSpeed * Time.deltaTime;
 
         // Mouse controls
@@ -75,15 +48,8 @@ public class CameraScroll : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
+            difference = new Vector3(difference.x, 0, 0);
             camTransform.position += difference;
         }
-    }
-
-    private void ZoomCamera()
-    {
-        float scrollDelta = Input.GetAxis("Mouse ScrollWheel");
-        float newZoom = cam.orthographicSize - scrollDelta * zoomSpeed * Time.deltaTime;
-        newZoom = Mathf.Clamp(newZoom, minZoom, maxZoom);
-        cam.orthographicSize = newZoom;
     }
 }
