bb3eb993ce87a0ce5341bd484c29dc051e13573a
AnahatM <anahat.mudgal@gmail.com>
2023-09-17 12:55:53 -0400

    new level select info page UI

--- GMTKGameJam2023/Assets/Levels/Scripts/LevelPage.cs
+++ GMTKGameJam2023/Assets/Levels/Scripts/LevelPage.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class LevelPage : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private GameObject objectIconPrefab;
+
     [Header("UI References")]
     [SerializeField] private TextMeshProUGUI levelNameText;
+    [SerializeField] private Transform levelCarsContainer;
+    [SerializeField] private Transform levelChickenContainer;
 
     [HideInInspector] public LevelInfoSO selecedLevel;
 
@@ -21,7 +27,23 @@ public class LevelPage : MonoBehaviour
     {
         levelNameText.text = selecedLevel.levelName;
 
+        CreateIcons(selecedLevel.carsInLevel, levelCarsContainer);
+        CreateIcons(selecedLevel.chickensInLevel, levelChickenContainer);
+
         gameObject.SetActive(true);
+    }
+
+    private void CreateIcons(ObjectInfo[] objects, Transform container)
+    {
+        foreach (ObjectInfo obj in objects)
+        {
+            // Create Icons Normally
+            LvlIcon newIcon = Instantiate(
+                objectIconPrefab,
+                container
+            ).GetComponent<LvlIcon>();
+            newIcon.SetImage(obj.objectIcon);
+        }
     }
 
     public void PlayLevel()
