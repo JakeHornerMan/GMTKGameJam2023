2a42147aca54266732adb6ef917339495003f155
Jake Horner <jakehorner19@gmail.com>
2024-02-23 20:19:45 +0000

    Jakes (#187)
    * gameManger has new start point
    
    * start button update
    
    * few changes
    
    * updates
    
    * updates
    
    * change
    
    * attempt to make chicken spawn work
    
    * updates
    
    * replay last scene
    
    * fixed chicken amounts on results
    
    * blah
    
    * move camera on main menu
    
    * updates
    
    * camera working on menu interactive
    
    * main menu
    
    * push
    
    * main menu work
    
    * save scene
    
    * blue print ui asset
    
    * blue prints selectors
    
    * move some scripts
    
    * image changes
    
    * care icon in wallet reload
    
    * node car wallet
    
    * new art
    
    * new edits
    
    * new node system
    
    * Game UI restructure
    
    * truck nerf
    
    * energy icons
    
    * chicken amounts
    
    * updates
    
    * Procedural chicken amounts
    
    * updates
    
    * fix for special waves
    
    * intensity settings per round
    
    * basic special chickens
    
    * updates to my scene
    
    * update
    
    * car changes
    
    * cancel select car btn
    
    * all chicken must be cleared for new round to start
    
    * ultimate button
    
    * changes
    
    * Renamed to Ultimate Manager
    
    * create ultimate script
    
    * ultimates are spawning
    
    * ultimates handle chicken collisions
    
    * freeze chicken missile
    
    * frozen chicken are now visable
    
    * updates to freeze missile size
    
    * ice bomb explosion
    
    * update
    
    * update freeze length
    
    * radials for ultimates
    
    * update to explosion animations
    
    * turbo chicken freeze now working
    
    * final updates
    
    * start of psychichen
    
    * phychic hen functionality
    
    * SOUNDS FOR PSYCHIC HEN
    
    ---------
    
    Co-authored-by: Jake Horner <jake.horner@ergogroup.ie>

---
+++ GMTKGameJam2023/Assets/Chicken/Scripts/Chicken Sub-Types/PortalController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalController : MonoBehaviour
+{
+    [SerializeField] private float removeTime = 3f;
+    public GameObject capturedVehicle;
+    private float portalDisappearAnimLength = 1f;
+    private float spawnAfterTime = 1f;
+    private Animator anim;
+    private SoundManager soundManager;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        soundManager = FindObjectOfType<SoundManager>();
+        // Debug.Log("Captured: " + capturedVehicle.name);
+        StartCoroutine(WaitAndDie(removeTime));
+
+        StartCoroutine(WaitAndSpawn(spawnAfterTime));
+    }
+
+    private IEnumerator WaitAndDie(float dieTime)
+    {
+        yield return new WaitForSeconds(dieTime - portalDisappearAnimLength);
+
+        anim.Play("PortalDisappear");
+
+        yield return new WaitForSeconds(portalDisappearAnimLength);
+
+        Destroy(gameObject);
+    }
+
+    private IEnumerator WaitAndSpawn(float spawnAfterTime)
+    {
+        yield return new WaitForSeconds(spawnAfterTime);
+
+        Vector3 spawnPos = new(transform.position.x, transform.position.y, 0);
+        GameObject spawnedVehicle = Instantiate(capturedVehicle, spawnPos, Quaternion.identity);
+        soundManager.PlayExitPortal();
+        spawnedVehicle.SetActive(true);
+    }
+}
