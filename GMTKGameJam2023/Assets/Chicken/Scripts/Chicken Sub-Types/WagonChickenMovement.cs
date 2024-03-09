b446a729419c27fbb8170fbb2db2bc27b42b36e6
AnahatM <anahat.mudgal@gmail.com>
2024-03-08 21:29:25 -0500

    wagon chicken places on road center

--- GMTKGameJam2023/Assets/Chicken/Scripts/Chicken Sub-Types/WagonChickenMovement.cs
+++ GMTKGameJam2023/Assets/Chicken/Scripts/Chicken Sub-Types/WagonChickenMovement.cs
@@ -11,7 +11,7 @@ public class WagonChickenMovement : ChickenMovement
     [Header("Wheelbarrow Chicken Values")]
     [SerializeField] private float speed = 1f;
     [SerializeField] private float substanceDurationSeconds = 20f;
-    private List<GameObject> affectedRoads;
+    private List<RoadHighlight> affectedRoads;
 
     private SoundManager soundManager;
 
@@ -22,7 +22,7 @@ public class WagonChickenMovement : ChickenMovement
 
     private void Start()
     {
-        affectedRoads = new List<GameObject>();
+        affectedRoads = new List<RoadHighlight>();
     }
 
     protected override void StartMovement()
@@ -44,15 +44,33 @@ public class WagonChickenMovement : ChickenMovement
             transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
         }
 
-        // See current road the chicken is on
-        
+        DropOnRoadCenter();
     }
 
-    private void DropSubstance()
+    private void DropOnRoadCenter()
+    {
+        // Raycast down from drop point to find the road that the chicken is currently on
+        Collider2D raycastHit = Physics2D.Raycast(dropPoint.position, Vector2.zero).collider;
+        RoadHighlight road = raycastHit?.GetComponent<RoadHighlight>();
+        if (
+            raycastHit != null // Hit something
+            && road != null // Is a road
+            && !affectedRoads.Contains(road) // Not already placed upon
+        )
+        {
+            // Drop horizontally centered on road
+            DropSubstance(new Vector2(road.transform.position.x, transform.position.y));
+            // Add to exclusion list
+            affectedRoads.Add(road);
+        }
+    }
+
+    private void DropSubstance(Vector2 dropPos)
     {
         GameObject substance = Instantiate(
             slowSubstancePrefab,
-            dropPoint.position,
+            // dropPoint.position,
+            dropPos,
             transform.rotation
         );
         Destroy(substance, substanceDurationSeconds);
