7055d7a536b4d2cf1703f924bb65f1609613a3e9
Jake Horner <jake.horner@ergogroup.ie>
2024-03-27 18:41:58 +0000

    generate list of road for map generation

--- GMTKGameJam2023/Assets/Environment/Scripts/MapGeneration.cs
+++ GMTKGameJam2023/Assets/Environment/Scripts/MapGeneration.cs
@@ -1,73 +1,152 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class MapGeneration : MonoBehaviour
 {
-    public GameObject[] roads;
+    public List<GameObject> laneTypes;
+    public int roadCount = 0;
+    public int busCount = 0;
+    public int dirtCount = 0;
+    public int grassCount = 0;
+    public int riverCount = 0;
+    public int pavementCount = 0;
 
-    public RoadRow[] roadRows = new RoadRow[12];
-
-    void Awake()
+    public List<GameObject> lanes;
+    
+    private void Awake()
     {
-        GenerateRoad();
+        // GenerateRoad();
     }
 
-    void GenerateRoad()
-    {
-        for (int i = 0; i <= 10; i++)
-        {
-            GameObject road = roads[Random.Range(1, roads.Length)];
-            Vector3 pos;
-            switch (i)
-            {
-                case 0:
-                    road = roads[2];
-                    pos = new Vector3(-13f, 0, 0);
-                    break;
-                case 1:
-                    pos = new Vector3(-10.5f, 0, 0);
-                    break;
-                case 2:
-                    pos = new Vector3(-8f, 0, 0);
-                    break;
-                case 3:
-                    pos = new Vector3(-5.5f, 0, 0);
-                    break;
-                case 4:
-                    pos = new Vector3(-3f, 0, 0);
-                    break;
-                case 5:
-                    pos = new Vector3(-0.5f, 0, 0);
-                    break;
-                case 6:
-                    pos = new Vector3(2f, 0, 0);
-                    break;
-                case 7:
-                    pos = new Vector3(4.5f, 0, 0);
-                    break;
-                case 8:
-                    pos = new Vector3(7f, 0, 0);
-                    break;
-                case 9:
-                    pos = new Vector3(9.5f, 0, 0);
-                    break;
-                case 10:
-                    pos = new Vector3(12f, 0, 0);
-                    break;
-                // case 11:
-                //      pos = new Vector3(14.5f,0,0);
-                // break;
-                // case 12:
-                //      pos = new Vector3(17f,0,0);
-                // break;
-                default:
-                    pos = new Vector3(0, 0, 0);
-                    break;
+    private void Start(){
+        PopulateLanesArray();
+    }
+
+    public void PopulateLanesArray(){
+        for (int i = 0; i < 12; i++){
+            GameObject laneSelected;
+            if(i == 0 || i == 11){
+                laneSelected = laneTypes[1];
+                lanes.Add(laneSelected);
+                continue;
             }
-            Instantiate(road, pos, Quaternion.identity);
+            int randomNumber = Random.Range(0, laneTypes.Count);
+            laneSelected = laneTypes[randomNumber];
+            lanes.Add(laneSelected);
+        }
+
+        LaneCount();
+
+        if(roadCount < 3){
+            do{
+                int randomNumber = Random.Range(0, 10);
+                lanes[randomNumber] = laneTypes[0];
+                roadCount++;
+            }
+            while(roadCount != 3);
+            LaneCount();
         }
     }
+
+    private void LaneCount(){
+        roadCount = 0;
+        busCount = 0;
+        dirtCount = 0;
+        grassCount = 0;
+        riverCount = 0;
+        pavementCount = 0;
+
+        foreach (GameObject item in lanes)
+        {
+            switch (item.name)
+            {
+                case "BusLane":
+                    busCount++;
+                break;
+                case "Dirt":
+                    dirtCount++;
+                break;
+                case "GrassLane":
+                    grassCount++;
+                break;
+                case "GrassLane_2":
+                    grassCount++;
+                break;
+                case "PavementLane":
+                    pavementCount++;
+                break;
+                case "River":
+                    riverCount++;
+                break;
+                case "RoadLane":
+                    roadCount++;
+                break;
+                default:
+                    Debug.Log("Error with Gameobject Lane!");
+                break;
+            }
+        }
+    }
+
+    
+
+    // void GenerateRoad()
+    // {
+    //     for (int i = 0; i <= 10; i++)
+    //     {
+    //         GameObject road = roads[Random.Range(1, roads.Length)];
+    //         Vector3 pos;
+    //         switch (i)
+    //         {
+    //             case 0:
+    //                 road = roads[2];
+    //                 pos = new Vector3(-13f, 0, 0);
+    //                 break;
+    //             case 1:
+    //                 pos = new Vector3(-10.5f, 0, 0);
+    //                 break;
+    //             case 2:
+    //                 pos = new Vector3(-8f, 0, 0);
+    //                 break;
+    //             case 3:
+    //                 pos = new Vector3(-5.5f, 0, 0);
+    //                 break;
+    //             case 4:
+    //                 pos = new Vector3(-3f, 0, 0);
+    //                 break;
+    //             case 5:
+    //                 pos = new Vector3(-0.5f, 0, 0);
+    //                 break;
+    //             case 6:
+    //                 pos = new Vector3(2f, 0, 0);
+    //                 break;
+    //             case 7:
+    //                 pos = new Vector3(4.5f, 0, 0);
+    //                 break;
+    //             case 8:
+    //                 pos = new Vector3(7f, 0, 0);
+    //                 break;
+    //             case 9:
+    //                 pos = new Vector3(9.5f, 0, 0);
+    //                 break;
+    //             case 10:
+    //                 pos = new Vector3(12f, 0, 0);
+    //                 break;
+    //             // case 11:
+    //             //      pos = new Vector3(14.5f,0,0);
+    //             // break;
+    //             // case 12:
+    //             //      pos = new Vector3(17f,0,0);
+    //             // break;
+    //             default:
+    //                 pos = new Vector3(0, 0, 0);
+    //                 break;
+    //         }
+    //         Instantiate(road, pos, Quaternion.identity);
+    //     }
+    // }
 }
 
 [System.Serializable]
--- GMTKGameJam2023/Assets/Player/Scripts/GameProgressionValues.cs
+++ GMTKGameJam2023/Assets/Player/Scripts/GameProgressionValues.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public static class GameProgressionValues
 {
     public static int sceneIndex = 0; //stores scene index
-    public static int RoundNumber = 20; //default : 1
+    public static int RoundNumber = 1; //default : 1
     public static float standardRoundTime = 30f; //default : 30f
-    public static int standardChickenAmountForStart = 30; //default : 30
+    public static int standardChickenAmountForStart = 40; //default : 30
     public static float standardChickenAmountMultiplier = 1.1f; //default : 1.1f
 
     public static void SetDefaultValues(){
