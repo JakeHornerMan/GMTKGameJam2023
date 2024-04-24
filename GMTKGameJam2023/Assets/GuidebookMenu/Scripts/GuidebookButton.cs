851c0dbcef03e2e6a92dee34a8e75b4c82a429b0
Anahat <61635745+AnahatM@users.noreply.github.com>
2024-04-24 17:31:22 -0400

    Anahat - Guidebook and Object Bluepritn and Car cursor and Sprite Tiling (#214)
    * create UFO Ultimate (NOT FUNCTIONAL)
    
    * new dev playground
    
    * questionable dirt sprite tiling improvements
    
    * car cursor info changes
    
    * Pavement changes
    
    * commit
    
    * Update PavementEdge.png
    
    * Basic blueprint UI and Sans serif font import
    
    * Scrolling buttons and sprites
    
    * finsihed car aspect of reference blueprint
    
    * UI Scalability
    
    * ultimate and chicken support for reference blueprint
    
    * Object type on blueprint
    
    Whether blueprint shows vehicle chicken or ultimate ability
    
    * Completed selection from grid system for cars blueprint
    
    * minor objectinfo chanegs
    
    * Commit
    
    * text color of description

---
+++ GMTKGameJam2023/Assets/GuidebookMenu/Scripts/GuidebookButton.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Shows icon of attached object.
+/// When clicked, brings up detailed blueprint view of that object.
+/// </summary>
+public class GuidebookButton : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] private Image btnIcon;
+
+    public enum ObjectType { Cars, Ultimates, Chicken }
+    private ObjectType objectType;
+
+    private Car assignedCar = null;
+    private ChickenMovement assignedChicken = null;
+    private Ultimate assignedUltimate = null;
+
+    // Creation run by GuidebookSelector.cs when this button is instantiated.
+    // Stores object along with type, and sets icon
+
+    // Variation for Car
+    public void Creation(Car assignedCar)
+    {
+        objectType = ObjectType.Cars;
+        this.assignedCar = assignedCar;
+        btnIcon.sprite = assignedCar.GetComponent<ObjectInfo>().objectIcon;
+    }
+
+    // Override for Ultimate
+    public void Creation(Ultimate assignedUltimate)
+    {
+        objectType = ObjectType.Ultimates;
+        this.assignedUltimate = assignedUltimate;
+        btnIcon.sprite = assignedUltimate.GetComponent<ObjectInfo>().objectIcon;
+    }
+
+    // Override for Chicken
+    public void Creation(ChickenMovement assignedChicken)
+    {
+        objectType = ObjectType.Chicken;
+        this.assignedChicken = assignedChicken;
+        btnIcon.sprite = assignedChicken.GetComponent<ObjectInfo>().objectIcon;
+    }
+
+    // Function when button is clicked
+    public void HandleClick()
+    {
+        ObjectBlueprint blueprint = FindObjectOfType<ObjectBlueprint>(true);
+        if (objectType == ObjectType.Cars)
+            blueprint.DisplayInfo(assignedCar);
+        if (objectType == ObjectType.Ultimates)
+            blueprint.DisplayInfo(assignedUltimate);
+        if (objectType == ObjectType.Chicken)
+            blueprint.DisplayInfo(assignedChicken);
+    }
+}
