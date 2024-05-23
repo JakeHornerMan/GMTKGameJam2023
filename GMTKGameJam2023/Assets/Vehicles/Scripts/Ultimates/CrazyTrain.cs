f90bbaa345ea68b78c1adaae095a0de877bb9acd
Jake Horner <jake.horner@ergogroup.ie>
2024-05-23 13:29:18 +0100

    changes to combo

--- GMTKGameJam2023/Assets/Vehicles/Scripts/Ultimates/CrazyTrain.cs
+++ GMTKGameJam2023/Assets/Vehicles/Scripts/Ultimates/CrazyTrain.cs
@@ -6,12 +6,9 @@ public class CrazyTrain : Ultimate
 {
     [Header("Train Movement")]
     [SerializeField] private Vector2 startPos = new(0, 0);
-    // private Vector3 endPos;
     [SerializeField] private float shakeIntensity = 5f;
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float xDestroyThreshold = -505f;
-
-    [Header("Train Ref")]
     private VehicleSpawner vehicleSpawner;
 
     private void Start()
@@ -19,7 +16,6 @@ public class CrazyTrain : Ultimate
         Debug.Log("Train Spawn!");
         vehicleSpawner = FindObjectOfType<VehicleSpawner>();
         transform.position = CalculateStartPosition();
-        // endPos = CalculateEndPosition();
         CameraShaker.instance.Shake(7.5f, shakeIntensity);
     }
 
@@ -32,8 +28,6 @@ public class CrazyTrain : Ultimate
     public void MoveTrain(){
         Vector3 direction = Vector3.left;
         transform.Translate(direction * moveSpeed * Time.deltaTime);
-
-        // CameraShaker.instance.Shake(7.5f, transform.position.y * shakeIntensity);
 
         if (transform.position.x <= xDestroyThreshold)
         {
@@ -51,8 +45,4 @@ public class CrazyTrain : Ultimate
         return new Vector3(startPos.x, inputPos.y, 0);
     }
 
-    // public Vector3 CalculateEndPosition(){
-    //     Vector3 inputPos = vehicleSpawner.inputPos;
-    //     return new Vector3(xDestroyThreshold, inputPos.y, 0);
-    // }
 }
