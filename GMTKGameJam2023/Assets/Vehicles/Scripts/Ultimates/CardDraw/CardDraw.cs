31ef018f8ce5b928436e528ce5b551fd0edb04a5
AnahatM <anahat.mudgal@gmail.com>
2024-06-02 15:03:55 -0400

    Work in progress card draw

--- GMTKGameJam2023/Assets/Vehicles/Scripts/Ultimates/CardDraw/CardDraw.cs
+++ GMTKGameJam2023/Assets/Vehicles/Scripts/Ultimates/CardDraw/CardDraw.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -17,10 +19,19 @@ using UnityEngine;
 public class CardDraw : Ultimate
 {
     [Header("Settings")]
-    [SerializeField] UltimateSelectableCard cardsList; // The 3 Cards (UI Elements)
-    [SerializeField] UltimateCardSO cardConfigs; // The Scriptable Objects for Possible Card Values
+    [SerializeField] List<UltimateSelectableCard> cardsList; // The 3 Cards (UI Elements)
+    [SerializeField] List<UltimateCardSO> cardConfigs; // The Scriptable Objects for Possible Card Values
     [Space]
     [SerializeField] private bool hideUIOnUltimateActivation = true;
+
+    private Transform chickenContainer;
+
+    private List<UltimateCardSO> shuffledConfigs;
+
+    private void Awake()
+    {
+        chickenContainer = GameObject.Find("ChickenContainer").transform;
+    }
 
     private void Start()
     {
@@ -28,16 +39,68 @@ public class CardDraw : Ultimate
         if (hideUIOnUltimateActivation)
         {
             // Find "GameCanvas" Object
+            GameObject.Find("GameCanvas").SetActive(false);
         }
+        Time.timeScale = 0f;
 
         // Assign Configs Randomly on cards List
+        shuffledConfigs = Shuffle(cardConfigs);
+        for (int i = 0; i < cardsList.Count; i++)
+        {
+            UltimateSelectableCard card = cardsList[i];
+            card.correspondingConfig = shuffledConfigs[i];
+        }
+    }
+
+    private List<T> Shuffle<T>(List<T> _list)
+    {
+        for (int i = 0; i < _list.Count; i++)
+        {
+            T temp = _list[i];
+            int randomIndex = Random.Range(i, _list.Count);
+            _list[i] = _list[randomIndex];
+            _list[randomIndex] = temp;
+        }
+
+        return _list;
     }
 
     public void HandleSelectedCard(UltimateSelectableCard cardClicked)
     {
+        // Flip every card to front
+        foreach (UltimateSelectableCard card in cardsList)
+        {
+            card.FlipCard(front: true);
+            card.SetUIActivation(true);
+        }
+
         if (cardClicked.correspondingConfig.cardType == UltimateCardSO.UltimateCardType.KillChickenFraction)
         {
+            if (cardClicked.correspondingConfig.chickenKillInterval == 0)
+            {
+                return;
+            }
             // Kill Chicken Amount Here using interval value from Scriptable Object
+            ChickenHealth[] chickens = chickenContainer.GetComponentsInChildren<ChickenHealth>();
+            for (int i = 0; i < chickens.Length; i++)
+            {
+                // Use Remainder to kill using interval
+                if ((i + 1) % cardClicked.correspondingConfig.chickenKillInterval == 0)
+                    chickens[i].TakeDamage(1000);
+            }
         }
+
+        ResumeAndEnd();
+    }
+
+    /// <summary>
+    /// Unpause Game, Re-enable UI, and Destroy Self
+    /// Run by Resume Button
+    /// </summary>
+    public void ResumeAndEnd()
+    {
+        GameObject.Find("GameCanvas").SetActive(true);
+        Time.timeScale = 1f;
+        Destroy(gameObject);
     }
 }
