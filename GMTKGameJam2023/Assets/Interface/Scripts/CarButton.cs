0209c4c12ec8764e6f53d8f3386edcb94eb27cf6
AnahatM <anahat.mudgal@gmail.com>
2024-06-30 15:47:30 -0400

    car select button token icon and coloring settings

--- GMTKGameJam2023/Assets/Interface/Scripts/CarButton.cs
+++ GMTKGameJam2023/Assets/Interface/Scripts/CarButton.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using UnityEngine.AI;
 
 public class CarButton : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] public TextMeshProUGUI tokenPriceText;
-    [SerializeField] public TextMeshProUGUI carWeightText; [SerializeField] private Image correspCarIcon;
+    [SerializeField] private Image tokenIcon;
+    [SerializeField] public TextMeshProUGUI carWeightText;
+    [SerializeField] private Image correspCarIcon;
     [SerializeField] private Image buttonOutline;
+    [SerializeField] private Image blueprintBG;
 
     [Header("Car Values")]
     [SerializeField] public Car correspondingCar;
@@ -18,9 +22,19 @@ public class CarButton : MonoBehaviour
     [Header("Colors")]
     [SerializeField] private Color positiveColor;
     [SerializeField] private Color negativeColor;
+    [SerializeField] private Sprite iconPositiveSprite;
+    [SerializeField] private Sprite iconNegativeSprite;
+    [SerializeField] private Sprite backgroundPositiveSprite;
+    [SerializeField] private Sprite backgroundNegativeSprite;
     [SerializeField] private Color positiveColorOutline;
     [SerializeField] private Color negativeColorOutline;
     [SerializeField] private Color standardCarColorOutline;
+
+    [Header("Coloration Settings")]
+    [SerializeField] private bool enableBackgroundColoration = false;
+    [SerializeField] private bool enableIconColoration = true;
+    [SerializeField] private bool enableOutlineColoration = true;
+    [SerializeField] private bool enableTextColoration = true;
 
     private VehicleSpawner vehicleSpawner;
     private GameManager gameManager;
@@ -48,32 +62,42 @@ public class CarButton : MonoBehaviour
     {
         if (correspondingCar)
         {
-            tokenPriceText.text = "$" + correspondingCar.carPrice.ToString("0");
+            tokenPriceText.text = correspondingCar.carPrice.ToString("0");
             carWeightText.text = correspondingCar.carHealth.ToString("0");
             correspCarIcon.sprite = correspondingCar.GetComponent<ObjectInfo>().objectIcon;
 
-            SetPriceColor();
             SetPriceColor();
         }
     }
 
     private void SetPriceColor()
     {
-        if(gameManager){
+        if (gameManager)
+        {
             bool enoughMoney = gameManager.tokens >= correspondingCar.carPrice;
-            tokenPriceText.color = enoughMoney ? positiveColor : negativeColor;
 
-            buttonOutline.color = enoughMoney ? positiveColorOutline: negativeColorOutline;
-            if(correspondingCar.name == "Standard Car"){
+            if (enableTextColoration)
+                tokenPriceText.color = enoughMoney ? positiveColor : negativeColor;
+            if (enableIconColoration)
+                tokenIcon.sprite = enoughMoney ? iconPositiveSprite : iconNegativeSprite; // Color lightning icon
+            if (enableBackgroundColoration)
+                blueprintBG.sprite = enoughMoney ? backgroundPositiveSprite : backgroundNegativeSprite; // Color background BG
+            if (enableOutlineColoration)
+                buttonOutline.color = enoughMoney ? positiveColorOutline : negativeColorOutline;
+
+            if (correspondingCar.name == "Standard Car")
+            {
                 buttonOutline.color = standardCarColorOutline;
             }
         }
-        if(tutorialManager){
+        if (tutorialManager)
+        {
             bool enoughMoney = tutorialManager.tokens >= correspondingCar.carPrice;
             tokenPriceText.color = enoughMoney ? positiveColor : negativeColor;
 
-            buttonOutline.color = enoughMoney ? positiveColorOutline: negativeColorOutline;
-            if(correspondingCar.name == "Standard Car"){
+            buttonOutline.color = enoughMoney ? positiveColorOutline : negativeColorOutline;
+            if (correspondingCar.name == "Standard Car")
+            {
                 buttonOutline.color = standardCarColorOutline;
             }
         }
