4d5d40b0e1accf3eac7cfdfef7927a7cd5d1ea42
TheJackXBL <79862200+TheJackXBL@users.noreply.github.com>
2024-06-30 18:18:35 +0100

    Major Shop Fixes - Swapping and Selling

--- GMTKGameJam2023/Assets/Interface/Scripts/BuyScreenCarSlot.cs
+++ GMTKGameJam2023/Assets/Interface/Scripts/BuyScreenCarSlot.cs
@@ -32,7 +32,7 @@ public class BuyScreenCarSlot : MonoBehaviour, IDropHandler, IPointerClickHandle
             if (eventData.pointerDrag.TryGetComponent<BuyScreenCar>(out car) || eventData.pointerDrag.TryGetComponent<BuyScreenUltimate>(out ultimate)) //eventData.pointerDrag = The car being held by the mouse
             {
                 //if (transform.childCount > 0)
-                if ((transform.GetComponentInChildren<BuyScreenCar>() != null) || (transform.GetComponentInChildren<BuyScreenUltimate>() != null))
+                if ((transform.GetComponentInChildren<BuyScreenCar>() != null) || (transform.GetComponentInChildren<BuyScreenUltimate>() != null)) //If the slot is not empty
                 {
 
 
@@ -44,42 +44,66 @@ public class BuyScreenCarSlot : MonoBehaviour, IDropHandler, IPointerClickHandle
                             {
                                 if (gameObject.name == "Ultimate Slot" && ultimate != null)
                                 {
-                                    eventData.pointerDrag.gameObject.transform.parent = transform;
-                                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-                                    eventData.pointerDrag.GetComponent<DragDrop>().canBePlaced = true;
+                                    ultimate.gameObject.transform.parent = transform;
+                                    ultimate.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                                    ultimate.GetComponent<DragDrop>().canBePlaced = true;
+                                    ultimate.GetComponent<DragDrop>().startingParent = transform;
 
                                     // Take away any money
                                     BuyScreenManager.instance.RemoveMoney(ultimate.correspondingUltimate.ultimateShopPrice);
+
+                                    ultimate.EnableParticles();
                                 }
                                 else if (gameObject.name != "Ultimate Slot" && car != null)
                                 {
-                                    eventData.pointerDrag.gameObject.transform.parent = transform;
-                                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-                                    eventData.pointerDrag.GetComponent<DragDrop>().canBePlaced = true;
+                                    car.gameObject.transform.parent = transform;
+                                    car.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                                    car.GetComponent<DragDrop>().canBePlaced = true;
+                                    car.GetComponent<DragDrop>().startingParent = transform;
 
                                     // Take away any money
-                                    if (car != null)
-                                        BuyScreenManager.instance.RemoveMoney(car.correspondingCar.carShopPrice);
+                                    BuyScreenManager.instance.RemoveMoney(car.correspondingCar.carShopPrice);
+                                    car.EnableParticles();
+
+
+
                                 }
 
-                                // Add pre-existing (pointerEnter) car to scrapyard
+                                // Sells the vehicle being replaced
                                 GameObject oldItem = eventData.pointerEnter.GetComponentInChildren<DragDrop>().gameObject;
-                                BuyScreenManager.instance.RemoveFromShop(oldItem);
+                                SellVehicle(oldItem);
                             }
                         }
 
                     }
                     else
                     {
-                        DragDrop childCar = eventData.pointerEnter.gameObject.GetComponentInChildren<DragDrop>();
-                        //Takes the pre-existing item and moves it to where your new car used to be 
-                        childCar.transform.SetParent(eventData.pointerDrag.GetComponent<DragDrop>().startingParent);
-                        childCar.gameObject.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                        if (eventData.pointerDrag.GetComponent<DragDrop>().startingParent.GetComponent<BuyScreenCarSlot>().slotType == SlotType.Roster && eventData.pointerEnter.GetComponentInChildren<DragDrop>().startingParent.GetComponent<BuyScreenCarSlot>().slotType == SlotType.Roster) // If both cars are currently in the roster
+                        {
+
+                            DragDrop heldCar = eventData.pointerDrag.gameObject.GetComponentInChildren<DragDrop>();
+                            DragDrop replacedCar = eventData.pointerEnter.gameObject.GetComponentInChildren<DragDrop>();
+
+                            GameObject oldParent = heldCar.startingParent.gameObject;
+                            GameObject newParent = replacedCar.startingParent.gameObject;
+
+                            //Takes the pre-existing item and moves it to where your new car used to be 
+                            replacedCar.transform.SetParent(oldParent.transform);
+                            replacedCar.gameObject.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+
+                            //Takes your held car and moves it to where the old car used to be
+                            heldCar.transform.SetParent(gameObject.transform);
+                            heldCar.gameObject.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+
+                            heldCar.GetComponent<DragDrop>().canBePlaced = true;
+
+                        }
+
                     }
                 }
                 else
                 {
-                    if (eventData.pointerDrag.GetComponent<DragDrop>().startingParent.GetComponent<BuyScreenCarSlot>().slotType == BuyScreenCarSlot.SlotType.CarShop)
+                    if (eventData.pointerDrag.GetComponent<DragDrop>().startingParent.GetComponent<BuyScreenCarSlot>().slotType == SlotType.CarShop)
                     {
                         if ((car != null && BuyScreenManager.instance.CheckMoneyAmount(car.correspondingCar.carShopPrice)) || (ultimate != null && BuyScreenManager.instance.CheckMoneyAmount(ultimate.correspondingUltimate.ultimateShopPrice))) // If the player has enough money to buy the car
                         {
@@ -88,22 +112,30 @@ public class BuyScreenCarSlot : MonoBehaviour, IDropHandler, IPointerClickHandle
                             {
                                 if (gameObject.name == "Ultimate Slot" && ultimate != null)
                                 {
-                                    eventData.pointerDrag.gameObject.transform.parent = transform;
-                                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-                                    eventData.pointerDrag.GetComponent<DragDrop>().canBePlaced = true;
+                                    ultimate.gameObject.transform.parent = transform;
+                                    ultimate.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                                    ultimate.GetComponent<DragDrop>().canBePlaced = true;
+                                    ultimate.GetComponent<DragDrop>().startingParent = transform;
+
 
                                     // Take away any money
                                     BuyScreenManager.instance.RemoveMoney(ultimate.correspondingUltimate.ultimateShopPrice);
+
+                                    ultimate.EnableParticles();
                                 }
                                 else if (gameObject.name != "Ultimate Slot" && car != null)
                                 {
-                                    eventData.pointerDrag.gameObject.transform.parent = transform;
-                                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-                                    eventData.pointerDrag.GetComponent<DragDrop>().canBePlaced = true;
+                                    car.gameObject.transform.parent = transform;
+                                    car.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                                    car.GetComponent<DragDrop>().canBePlaced = true;
+                                    car.GetComponent<DragDrop>().startingParent = transform;
 
                                     // Take away any money
                                     if (car != null)
+                                    {
                                         BuyScreenManager.instance.RemoveMoney(car.correspondingCar.carShopPrice);
+                                        car.EnableParticles();
+                                    }
                                 }
                                 
                                     
@@ -122,38 +154,14 @@ public class BuyScreenCarSlot : MonoBehaviour, IDropHandler, IPointerClickHandle
                     {
                         if (slotType == SlotType.Scrapyard)
                         {
-                            if (ultimate != null)
-                            {
-                                eventData.pointerDrag.gameObject.transform.parent = transform;
-                                eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-                                eventData.pointerDrag.GetComponent<DragDrop>().canBePlaced = true;
-
-                                // Add Money
-                                BuyScreenManager.instance.AddMoney(25);
-
-                                //DESTROY THE VEHICLE
-                                Destroy(eventData.pointerDrag.gameObject);
-                            }
-                            else if (car != null)
-                            {
-                                eventData.pointerDrag.gameObject.transform.parent = transform;
-                                eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-                                eventData.pointerDrag.GetComponent<DragDrop>().canBePlaced = true;
-
-                                // Add Money
-                                if (car.correspondingCar.carShopPrice < 5)
-                                {
-                                    BuyScreenManager.instance.AddMoney(3);
-                                }
-                                else
-                                {
-                                    BuyScreenManager.instance.AddMoney(5);
-                                }
-
-                                //DESTROY THE VEHICLE
-                                Destroy(eventData.pointerDrag.gameObject);
-                            }
+                            SellVehicle(eventData.pointerDrag);
                         }
+                        else if (slotType == SlotType.Roster)
+                        {
+                            eventData.pointerDrag.gameObject.transform.parent = transform;
+                            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                            eventData.pointerDrag.GetComponent<DragDrop>().canBePlaced = true;
+                        }    
                         else
                         {
                             eventData.pointerDrag.gameObject.transform.parent = eventData.pointerDrag.GetComponent<DragDrop>().startingParent;
@@ -208,6 +216,50 @@ public class BuyScreenCarSlot : MonoBehaviour, IDropHandler, IPointerClickHandle
                 }
             }
         }
+    }
+
+    void SellVehicle(GameObject vehicle)
+    {
+
+        BuyScreenCar car = null;
+        BuyScreenUltimate ultimate = null;
+
+
+        if (vehicle.TryGetComponent<BuyScreenCar>(out car) || vehicle.TryGetComponent<BuyScreenUltimate>(out ultimate))
+        {
+            if (ultimate != null)
+            {
+                ultimate.gameObject.transform.parent = transform;
+                ultimate.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                ultimate.GetComponent<DragDrop>().canBePlaced = true;
+
+                // Add Money
+                BuyScreenManager.instance.AddMoney(25);
+
+                //DESTROY THE VEHICLE
+                Destroy(ultimate.gameObject);
+            }
+            else if (car != null)
+            {
+                car.gameObject.transform.parent = transform;
+                car.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                car.GetComponent<DragDrop>().canBePlaced = true;
+
+                // Add Money
+                if (car.correspondingCar.carShopPrice < 5)
+                {
+                    BuyScreenManager.instance.AddMoney(3);
+                }
+                else
+                {
+                    BuyScreenManager.instance.AddMoney(5);
+                }
+
+                //DESTROY THE VEHICLE
+                Destroy(car.gameObject);
+            }
+        }
+        
     }
 
     public void UpdateBGColour()
