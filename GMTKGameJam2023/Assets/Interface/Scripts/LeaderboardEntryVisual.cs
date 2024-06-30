917a5da51c2400656ebf1dde606bfedc224632af
Anahat <61635745+AnahatM@users.noreply.github.com>
2024-06-30 13:16:39 -0400

    Leaderboards UI - Anahat (#266)
    * leaderboard UI
    
    * ui polish and leaderboard sprites polish

--- GMTKGameJam2023/Assets/Interface/Scripts/LeaderboardEntryVisual.cs
+++ GMTKGameJam2023/Assets/Interface/Scripts/LeaderboardEntryVisual.cs
@@ -8,21 +8,46 @@ using System;
 
 public class LeaderboardEntryVisual : MonoBehaviour
 {
+
+    [Header("UI References")]
+    [SerializeField] private Image entrySpriteImg;
+    [SerializeField] private GameObject selfIcon;
+    [SerializeField] private GameObject trophyIcon;
     [SerializeField] private TextMeshProUGUI rankText;
     [SerializeField] private TextMeshProUGUI usernameText;
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private Color playerColor;
 
+    [Header("Sprite images")]
+    [SerializeField] private Sprite normalEntrySprite;
+    [SerializeField] private Sprite firstPlaceEntrySprite;
+    [SerializeField] private Sprite selfEntrySprite;
+
     public void SetValues(LeaderboardEntry leaderboardEntry)
     {
+        trophyIcon.SetActive(false);
+        selfIcon.SetActive(false);
+
         rankText.text = leaderboardEntry.GlobalRank.ToString() + ".";
         usernameText.text = leaderboardEntry.UserName.ToString();
         scoreText.text = leaderboardEntry.Score.ToString();
 
-        if(leaderboardEntry.IsPlayer){
+        entrySpriteImg.sprite = normalEntrySprite;
+
+        if (leaderboardEntry.IsPlayer)
+        {
             rankText.color = playerColor;
             usernameText.color = playerColor;
             scoreText.color = playerColor;
+
+            entrySpriteImg.sprite = selfEntrySprite;
+            selfIcon.SetActive(true);
+        }
+
+        if (leaderboardEntry.GlobalRank == 1)
+        {
+            entrySpriteImg.sprite = firstPlaceEntrySprite;
+            trophyIcon.SetActive(true);
         }
     }
 }
