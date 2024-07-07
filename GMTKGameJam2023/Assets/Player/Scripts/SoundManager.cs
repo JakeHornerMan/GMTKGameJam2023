887602d67403c0e88eda0ae10026cfdb865bb44d
AnahatM <anahat.mudgal@gmail.com>
2024-07-07 13:12:59 -0400

    Update SoundManager.cs

--- GMTKGameJam2023/Assets/Player/Scripts/SoundManager.cs
+++ GMTKGameJam2023/Assets/Player/Scripts/SoundManager.cs
@@ -2,7 +2,6 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
