fdbba8b643f3b67c1e2b027807702cc234d51572
AnahatM <anahat.mudgal@gmail.com>
2024-07-09 11:34:20 -0400

    Popup UI flow (skipping not functional)

--- GMTKGameJam2023/Assets/Interface/Scripts/RoundSkipPopup.cs
+++ GMTKGameJam2023/Assets/Interface/Scripts/RoundSkipPopup.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class RoundSkipPopup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("UI References")]
+    [SerializeField] private GameObject popupUI;
+    [SerializeField] private TextMeshProUGUI headerText = null;
+    [SerializeField] public Button skipRoundButton = null;
+    [SerializeField] public Button roundOneButton = null;
+    [SerializeField] private TextMeshProUGUI skipRoundButtonText = null;
+
+    [Header("Text Settings")]
+    [SerializeField] private string headerTextTemplate = "You have already made it to <color=#3DDD7D>round $</color>.";
+    [SerializeField] private string skipButtonTextTemplate = "Skip to round $";
+    [SerializeField] private char numberKey = '$';
+
+    private void Start()
     {
-        
+        // Hide UI by default
+        popupUI.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenPopupUI(int roundToSkip)
     {
-        
+        // Update text with round number.
+        headerText.text = headerTextTemplate.Replace(numberKey.ToString(), roundToSkip.ToString());
+        skipRoundButtonText.text = skipButtonTextTemplate.Replace(numberKey.ToString(), roundToSkip.ToString());
+
+        popupUI.SetActive(true);
     }
 }
