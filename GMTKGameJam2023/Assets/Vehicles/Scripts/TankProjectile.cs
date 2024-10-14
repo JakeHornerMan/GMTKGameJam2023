5b5e3461677ec01f075d9a6563d6a55d83fd7bc8
Jake Horner <jakehorner19@gmail.com>
2024-10-14 18:25:54 +0100

    Update TankProjectile.cs

--- GMTKGameJam2023/Assets/Vehicles/Scripts/TankProjectile.cs
+++ GMTKGameJam2023/Assets/Vehicles/Scripts/TankProjectile.cs
@@ -16,7 +16,7 @@ public class TankProjectile : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 target;
-    public bool isFired = false;
+    private bool isFired = false;
 
     private void Awake()
     {
