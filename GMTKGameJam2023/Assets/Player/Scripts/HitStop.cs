6e2543f8592737730e2b4c63a62314b3afdbeb52
TheJackXBL <79862200+TheJackXBL@users.noreply.github.com>
2024-10-18 22:33:19 +0100

    HitStop should never freeze the game now

--- GMTKGameJam2023/Assets/Player/Scripts/HitStop.cs
+++ GMTKGameJam2023/Assets/Player/Scripts/HitStop.cs
@@ -1,12 +1,14 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HitStop : MonoBehaviour
 {
     private float currentTimeScale;
+    private Coroutine hitStopRoutine;
 
     [HideInInspector] public static HitStop instance;
+
+    public Pause pause;
 
     private void Start()
     {
@@ -15,17 +17,46 @@ public class HitStop : MonoBehaviour
 
     public void StartHitStop(float hitStopLength)
     {
-        StartCoroutine(HitStopCoroutine(hitStopLength));
+        if (hitStopRoutine != null)
+        {
+            StopCoroutine(hitStopRoutine);
+        }
+        hitStopRoutine = StartCoroutine(HitStopCoroutine(hitStopLength));
     }
 
     private IEnumerator HitStopCoroutine(float hitStopLength)
     {
-        currentTimeScale = Time.timeScale;
+        // If GetMeOut coroutine is active, kill it, then start it up from the beginning
+        if (hitStopRoutine != null)
+        {
+            StopCoroutine(hitStopRoutine);
+        }
+        hitStopRoutine = StartCoroutine(GetMeOutOfTheHitStop());
 
+        currentTimeScale = Time.timeScale;
         Time.timeScale = 0.0f;
 
         yield return new WaitForSecondsRealtime(hitStopLength);
 
         Time.timeScale = currentTimeScale;
+    }
+
+    private IEnumerator GetMeOutOfTheHitStop()
+    {
+        float timeInHitStop = 0.0f;
+
+        while (Time.timeScale == 0.0f && !pause.isPaused)
+        {
+            timeInHitStop += Time.unscaledDeltaTime;
+
+            // If time.timeScale has been zero for over a second, force it to 1.0f unless the game is paused
+            if (timeInHitStop > 1.0f)
+            {
+                Time.timeScale = 1.0f;
+                yield break; // exit the coroutine once time scale is restored
+            }
+
+            yield return null;
+        }
     }
 }
